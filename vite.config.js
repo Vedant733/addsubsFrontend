0d310a81a965fa72b885518c48f93a6945567b45
Vedant Barge <bargevedant@gmail.com>
2024-01-11 21:45:36 +0530

    api v3

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://imaginative-salmiakki-3c4991.netlify.app/",
-        secure: false,
-      },
-    },
-  },
   plugins: [react()],
 })
